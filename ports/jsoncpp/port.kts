e73775740d0f27c85573cbc2f7a8a551fed09fd5
Dan Albert <danalbert@google.com>
2020-02-10 12:38:57 -0800

    Downgrade jsoncpp to 1.8.4.
    The 1.9.x releases are pre-releases. 1.8.4 is the latest stable
    release.
    
    Test: presubmit
    Bug: None
    Change-Id: I16602d5b081d075de786250e833911d7d9b0c769

--- ports/jsoncpp/port.kts
+++ ports/jsoncpp/port.kts
@@ -20,7 +20,7 @@ import java.io.File
 
 object : MesonPort() {
     override val name = "jsoncpp"
-    override val version = "1.9.1"
+    override val version = "1.8.4"
     override val mavenVersion = "$version-alpha-1"
 
     override val license = License(
