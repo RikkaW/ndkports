07b1972462a5cb65c7494d96c66f9e67745502bc
Dan Albert <danalbert@google.com>
2020-04-21 13:02:52 -0700

    Update OpenSSL to 1.1.1g.
    Bug: https://github.com/google/prefab/issues/95
    Change-Id: Id4a476e32b236f8a8dbc240d2ad7cd9620d3ce3e

--- ports/openssl/port.kts
+++ ports/openssl/port.kts
@@ -20,9 +20,9 @@ import java.io.File
 
 object : Port() {
     override val name = "openssl"
-    override val version = "1.1.1f"
+    override val version = "1.1.1g"
     override val mavenVersion = "$version-alpha-1"
-    override val prefabVersion = CMakeCompatibleVersion(1, 1, 1, 6)
+    override val prefabVersion = CMakeCompatibleVersion(1, 1, 1, 7)
 
     override val license = License(
         "Dual OpenSSL and SSLeay License",
